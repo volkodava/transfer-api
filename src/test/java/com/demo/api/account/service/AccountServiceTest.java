b27173e58e1d142e21baa8e0768d0a9e03e8372b
Volkodav Anatolii, DBU-ION (EXT) <Anatolii.Volkodav@swisscom.com>
2020-03-08 16:44:34 +0100

    account test cleanup

--- src/test/java/com/demo/api/account/service/AccountServiceTest.java
+++ src/test/java/com/demo/api/account/service/AccountServiceTest.java
@@ -1,6 +1,9 @@
 package com.demo.api.account.service;
 
-import com.demo.api.account.AccountModule;
+import com.demo.api.account.model.Account;
+import com.demo.api.account.repository.AccountRepository;
+import com.demo.api.account.validator.AccountValidator;
+import com.demo.api.account.validator.DebitAccountValidator;
 import com.demo.api.model.AccountId;
 import com.demo.common.InvalidDataException;
 import com.google.inject.AbstractModule;
@@ -13,10 +16,16 @@ import java.math.BigDecimal;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 @RunWith(GuiceJUnitRunner.class)
 @GuiceJUnitRunner.GuiceModules(AccountServiceTest.TestModule.class)
 public class AccountServiceTest {
+
+    @Inject
+    private AccountRepository accountRepository;
 
     @Inject
     private AccountService accountService;
@@ -32,6 +41,7 @@ public class AccountServiceTest {
         // Then
         assertThat(sourceAccountId, notNullValue());
         assertThat(sourceAccountId.getValue(), not(isEmptyOrNullString()));
+        verify(accountRepository, times(1)).save(eq(sourceAccountId), any(Account.class));
     }
 
     @Test
@@ -45,20 +55,31 @@ public class AccountServiceTest {
         // Then
         assertThat(sourceAccountId, notNullValue());
         assertThat(sourceAccountId.getValue(), not(isEmptyOrNullString()));
+        verify(accountRepository, times(1)).save(eq(sourceAccountId), any(Account.class));
     }
 
-    @Test(expected = InvalidDataException.class)
+    @Test
     public void shouldFailWhenCreateAccountWithNegativeBalance() {
         // Given
         BigDecimal positiveBalance = BigDecimal.ONE.negate();
 
         // When
-        accountService.createNew(positiveBalance); // exception should be thrown
+        try {
+            accountService.createNew(positiveBalance); // exception should be thrown
+            fail("Expected exception to be thrown.");
+        } catch (InvalidDataException expected) {
+        }
+
+        // Then
+        verify(accountRepository, never()).save(any(AccountId.class), any(Account.class));
     }
 
     public static class TestModule extends AbstractModule {
         protected void configure() {
-            install(new AccountModule());
+            // mock
+            bind(AccountRepository.class).toInstance(mock(AccountRepository.class));
+            bind(AccountValidator.class).to(DebitAccountValidator.class);
+            bind(AccountService.class).to(InMemoryAccountService.class);
         }
     }
 }
